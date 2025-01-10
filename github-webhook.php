e1c0ed793b431270723d342ad1684cf4d6d49d11
Strickland <strickland.jonathan.ext@siemens-energy.com>
2025-01-10 13:15:14 -0500

    updated github pull api

--- github-webhook.php
+++ github-webhook.php
@@ -1,6 +1,7 @@
 <?php
 $secret = 'bbl';
-$shellScript = '/github_pull/pull-prod.sh'; // Path to shell script
+$shellScript = '/var/github_pull/pull-prod.sh'; // Path to shell script
+$shellScriptDev = '/var/github_pull/pull-dev.sh'; // Path to shell script (dev)
 
 // Get the payload and verify the signature
 $payload = file_get_contents('php://input');
@@ -13,6 +14,11 @@ if (hash_equals($signature, $_SERVER['HTTP_X_HUB_SIGNATURE_256'])) {
         shell_exec($shellScript);
         http_response_code(200);
         echo 'Script executed successfully';
+    } elseif ($data['ref'] === 'refs/heads/dev') {
+        // dev branch pull, run dev pull
+        shell_exec($shellScriptDev);
+        http_response_code(200);
+        echo 'Script executed successfully for dev pull';
     } else {
         http_response_code(400);
         echo 'Invalid branch';
