cdf40c53ef0ffe433f8353cd5b983816ea416fc5
cataon03 <catalinaocampon03@gmail.com>
2025-01-15 14:47:17 -0500

    checked username for duplicates

--- LAMPAPI/AddUser.php
+++ LAMPAPI/AddUser.php
@@ -3,22 +3,37 @@
 	
 	$firstname = $inData["FirstName"];
 	$lastname = $inData["LastName"];
-	$login = $inData["Login"];
-	$password = $inData["Password"];
+	$login = $inData["Login"]; //only username will be lowercased by frontend
+	$password = $inData["Password"]; 
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstname, $lastname, $login, $password);
+		returnWithError($conn->connect_error);
+
+	} else {
+		$stmt = $conn->prepare("SELECT COUNT(*) FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
 		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		if ($result == 0) {
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $firstname, $lastname, $login, $password);
+			$stmt->execute();
+			
+			if ($stmt->affected_rows > 0) {
+				returnWithError("");
+			} else {
+				returnWithError("failed to insert the user");
+			}
+		} 
+		else { //login already exists!
+			returnWithError("username '$login' is already taken.");
+		}
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	
